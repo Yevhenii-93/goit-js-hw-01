d9448c524763dd3578cd7e9c6dd5d7484a75c5b0
Yevhenii <petrushkov093@gmail.com>
2024-12-24 17:53:00 +0200

    add prettier

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,8 @@
-"use strict";
+'use strict';
 
 function makeTransaction(quantity, pricePerDroid) {
-    const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  const totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
